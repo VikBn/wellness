961c5d3a6e6eac40e03df0a6cec7bef5d403504a
VikBn <32108055+VikBn@users.noreply.github.com>
2018-10-29 10:38:04 +0200

    Update index.js

--- wellness/js/index.js
+++ wellness/js/index.js
@@ -1,28 +1,3 @@
-// $(document).ready(function() {
-//   $('select').niceSelect();
-// });
-
-
-// $('input').checks({
-//   class_radio: 'ico-radio',
-//   class_checkbox: 'ico-checkbox',
-//   class_checked: 'sed',
-//   class_disabled: 'disabled',
-//   types: ['radio', 'checkbox']
-// });
-
-
-// var label = $('.sidebar__confirm').find('label');
-// console.log(label);
-//
-//
-// label.on('click', function(){
-//   var input = $(this).parent().find('.ico-checkbox');
-//   console.log(input);
-//   input.toggleClass('sed');
-// })
-
-
 $(document).ready(function(){
   $('[type="checkbox"]').checkBo();
 });
@@ -32,8 +7,9 @@ $(function(){
   var proInner = $('#pro-block');
   var enterpriseInner = $('#enterprise-block');
 
-  // problock
+  // pro block
   var proCheckbox = proInner.find('[type="checkbox"]');
+
   proCheckbox.on('change', function(){
     var btn = proInner.find('[data-type="pro"]');
     if (proCheckbox.is(':checked')) {
@@ -43,8 +19,9 @@ $(function(){
     }
   })
 
-  // enterprise
+  // enterprise block
   var enterCheckbox = enterpriseInner.find('[type="checkbox"]');
+
   enterCheckbox.on('change', function(){
     var btn = enterpriseInner.find('[data-type="enterprise"]');
     if (enterCheckbox.is(':checked')) {
@@ -53,28 +30,4 @@ $(function(){
       btn.addClass('disabled')
     }
   })
-
-
-
-
 })
-
-// btn.on('click', function(){
-//
-//   if ($(this).data('type') == 'pro') {
-//
-//   }
-//
-//   if ($(this).data('type') == 'enterprise') {
-//     let allCheckbox = enterpriseInner.find('[type="checkbox"]');
-//     if (allCheckbox.is(':checked')) {
-//       console.log('nice');
-//     }
-//
-//   }
-
-
-
-
-
-// })
